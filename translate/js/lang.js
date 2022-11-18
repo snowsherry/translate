ce802a479d4804982637bd7b61f59702a2a29e82
xueyj-a <xueyj-a@glodon.com>
2022-11-18 09:36:57 +0800

    fix 更新Api接口地址

--- translate/js/lang.js
+++ translate/js/lang.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 console.log('process.env.NODE_ENV',process.env.NODE_ENV)
 let apiUrl;
 if(process.env.NODE_ENV=="production"||1){
-   apiUrl="http://node.itianhuihui.com/translate/lang";
+   apiUrl="http://node.huokele.com/lang";
 }else{
     apiUrl="http://localhost:3000/lang";
 }
